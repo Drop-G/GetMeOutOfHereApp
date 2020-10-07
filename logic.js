2279a16517f4cda69d14a903b4624909fdb4c8b3
Ted Peters <44210809+Drop-G@users.noreply.github.com>
2020-10-06 21:46:28 -0500

    added fullsize map and drop down functionality

--- logic.js
+++ logic.js
@@ -1,10 +1,16 @@
-
+// intitalize maps
 mapboxgl.accessToken = 'pk.eyJ1IjoiZHJvcC1nIiwiYSI6ImNrZnd3OTd6azFvMWkydG10aGN2Z2Q2MnUifQ.EOjRwzS_WYEK5wOfXc32sQ';
-  var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11'
-  });
+var map = new mapboxgl.Map({
+container: 'map', // container id
+style: 'mapbox://styles/mapbox/outdoors-v11', //stylesheet location
+center: [11.255, 43.77], // starting position
+zoom: 13 // starting zoom
+});
+ 
+map.addControl(new mapboxgl.FullscreenControl());
 
-
+// dropdown menu
+$('.dropdown-trigger').dropdown();
+$('.dropdown-trigger').close();
 
 
